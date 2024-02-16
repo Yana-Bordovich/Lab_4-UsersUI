cdbf5441c7e9143ac3f7e420174e25c4c82ac44f
Yana-Bordovich <98753081+Yana-Bordovich@users.noreply.github.com>
2024-02-16 20:06:09 +0300

    AddDrawer.kt complete

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.topic2.android.notes
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
 import com.topic2.android.notes.viewmodel.MainViewModel
 import com.topic2.android.notes.viewmodel.MainViewModelFactory
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.rememberCoroutineScope
+import com.topic2.android.notes.routing.Screen
+import com.topic2.android.notes.theme.NotesTheme
+import com.topic2.android.notes.ui.components.AppDrawer
+import com.topic2.android.notes.ui.components.Note
+import kotlinx.coroutines.launch
 
 /**
  * Main activity приложения.
@@ -18,11 +28,35 @@ class MainActivity : AppCompatActivity() {
       (application as NotesApplication).dependencyInjector.repository
     )
   })
-
+  @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContent {
+      NotesTheme {
+        val coroutineScope = rememberCoroutineScope()
+        val scaffoldState: ScaffoldState = rememberScaffoldState()
+
+        Scaffold(
+          scaffoldState = scaffoldState,
+          drawerContent = {
+            AppDrawer(
+              currentScreen = Screen.Notes,
+              closeDrawerAction = {
+                coroutineScope.launch {
+                  scaffoldState.drawerState.close()
+                }
+              }
+            )
+
+          },
+          content = {
+            Note()
+          }
+        )
+
+
+      }
 
     }
   }
