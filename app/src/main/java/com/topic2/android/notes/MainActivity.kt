6e76529e474fdaedbd6b72286f88cf9e28dd73e9
Yana-Bordovich <98753081+Yana-Bordovich@users.noreply.github.com>
2024-02-21 18:37:32 +0300

    Main Completed

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -5,17 +5,22 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.ExperimentalMaterialApi
 import com.topic2.android.notes.viewmodel.MainViewModel
 import com.topic2.android.notes.viewmodel.MainViewModelFactory
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Surface
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import com.topic2.android.notes.routing.NotesRouter
 import com.topic2.android.notes.routing.Screen
 import com.topic2.android.notes.theme.NotesTheme
-import com.topic2.android.notes.ui.components.components.components.AppDrawer
-import com.topic2.android.notes.ui.components.Note
 import kotlinx.coroutines.launch
+import com.topic2.android.notes.ui.screens.NotesScreen
+import com.topic2.android.notes.ui.screens.SaveNoteScreen
+import com.topic2.android.notes.ui.screens.TrashScreen
 
 /**
  * Main activity приложения.
@@ -28,35 +33,28 @@ class MainActivity : AppCompatActivity() {
       (application as NotesApplication).dependencyInjector.repository
     )
   })
+  @ExperimentalMaterialApi()
   @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContent {
       NotesTheme {
-        val coroutineScope = rememberCoroutineScope()
-        val scaffoldState: ScaffoldState = rememberScaffoldState()
-
-        Scaffold(
-          scaffoldState = scaffoldState,
-          drawerContent = {
-            AppDrawer(
-              currentScreen = Screen.Notes,
-              closeDrawerAction = {
-                coroutineScope.launch {
-                  scaffoldState.drawerState.close()
-                }
-              }
-            )
-
-          },
-          content = {
-          }
-        )
-
-
+        MainActivityScreen(viewModel = viewModel)
       }
 
+    }
+  }
+}
+
+@Composable
+@ExperimentalMaterialApi
+private fun MainActivityScreen(viewModel: MainViewModel) {
+  Surface {
+    when (NotesRouter.currentScreen) {
+      is Screen.Notes -> NotesScreen(viewModel)
+      is Screen.SaveNote -> SaveNoteScreen(viewModel)
+      is Screen.Trash -> TrashScreen(viewModel)
     }
   }
 }
